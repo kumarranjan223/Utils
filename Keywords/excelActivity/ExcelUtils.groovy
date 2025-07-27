2b1d04a51391aab6d1dfa64397c7f067dc059640
kumarranjan223 <kumarranjan223@gmail.com>
2025-07-27 21:29:19 +0530

    Updated Keyword

--- Keywords/excelActivity/ExcelUtils.groovy
+++ Keywords/excelActivity/ExcelUtils.groovy
@@ -1,8 +1,12 @@
 package excelActivity
 
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+
 import org.apache.poi.xssf.usermodel.*
 
 import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testdata.TestDataFactory
 
 class ExcelUtils {
 
@@ -46,4 +50,35 @@ class ExcelUtils {
 		workbook.close()
 	}
 	
+	@Keyword
+    static String readData_from_Excel(String filePath, String sheetName, String columnHeader, int rowNumber) {
+        FileInputStream fis = new FileInputStream(filePath)
+        XSSFWorkbook workbook = new XSSFWorkbook(fis)
+        XSSFSheet sheet = workbook.getSheet(sheetName)
+
+        XSSFRow headerRow = sheet.getRow(0)
+        XSSFRow dataRow = sheet.getRow(rowNumber)
+
+        int colIndex = -1
+        for (int i = 0; i < headerRow.getLastCellNum(); i++) {
+            if (headerRow.getCell(i).toString().trim() == columnHeader) {
+                colIndex = i
+                break
+            }
+        }
+
+        String value = dataRow.getCell(colIndex).toString()
+
+        workbook.close()
+        fis.close()
+
+        return value
+    }
+	
+	@Keyword
+	static String readData_from_DataFile(String DataFileName, String columnName, int rowNumber) {
+		def data = TestDataFactory.findTestData(DataFileName)
+		String value = data.getValue(columnName, rowNumber)
+		return value
+	}
 }
--- Scripts/DataSheetTest/Script1753525347456.groovy
+++ Scripts/DataSheetTest/Script1753525347456.groovy
@@ -11,6 +11,12 @@ println(user)
 
 String filePath = RunConfiguration.getProjectDir()+File.separator+'Data Files'+File.separator+'LoginData.xlsx'
 
-ExcelUtils.writeToCell(filePath, 'Sheet1', 'Sotel', 2, user)
+ExcelUtils.writeToCell(filePath, 'Sheet1', 'Name', 1, user)
 
+def fetchdata = ExcelUtils.readData_from_DataFile('LoginDataSheet', 'Subject', 2)
 
+println('Subject is : '+fetchdata)
+
+String fetch2 = ExcelUtils.readData_from_Excel(filePath, 'Sheet1', 'Marks', 3)
+
+println('Marks is : '+fetch2)
